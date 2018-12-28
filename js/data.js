11dc4cfea662a0f7f251a032b50f82a9ef6fa373
YagodinAleksandr <admin@MacBook-Pro-Macbook-2.local>
2018-12-28 16:08:35 +0200

    last update 2018.28 v1.3

--- js/data.js
+++ js/data.js
@@ -28,15 +28,120 @@ var data = [{
 	available: true
 },{
 	id: "004",
+	name: "ДСП 8548 SN Файнлайн мокка",
+	dimension: "2800 х 1002 мм.",
+	thickness: 18,
+	producer: "Kronospan",
+	description: "Деталь у хорошому стані",
+	price: "550 грн.",
+	available: true
+},{
+	id: "005",
+	name: "ДВП 0101 Білий",
+	dimension: "2010 х 770 мм.",
+	thickness: 3,
+	producer: "Kronospan",
+	description: "Деталь у хорошому стані",
+	price: "90 грн.",
+	available: true
+},{
+	id: "006",
+	name: "ДВП 0101 Білий",
+	dimension: "2280 х 1200 мм.",
+	thickness: 3,
+	producer: "Kronospan",
+	description: "Деталь у хорошому стані",
+	price: "160 грн.",
+	available: true
+},{
+	id: "007",
+	name: "ДСП Беж",
+	dimension: "2280 х 695 мм.",
+	thickness: 18,
+	producer: "Noname",
+	description: "Деталь у хорошому стані",
+	price: "360 грн.",
+	available: true
+},{
+	id: "008",
+	name: "W908 SМ Білий базовий",
+	dimension: "2080 х 230 мм.",
+	thickness: 16,
+	producer: "Egger",
+	description: "Деталь у хорошому стані",
+	price: "120 грн.",
+	available: true
+},{
+	id: "009",
+	name: "ДСП",
+	dimension: "2800 х 570 мм.",
+	thickness: 16,
+	producer: "",
+	description: "Деталь у хорошому стані",
+	price: " грн.",
+	available: false
+},{
+	id: "010",
+	name: "ДСП Белый",
+	dimension: "2800 х 943 мм.",
+	thickness: 18,
+	producer: "",
+	description: "Деталь у хорошому стані",
+	price: " грн.",
+	available: false
+},{
+	id: "011",
+	name: "",
+	dimension: "2800 х 586 мм.",
+	thickness: 3,
+	producer: "",
+	description: "Деталь у хорошому стані",
+	price: " грн.",
+	available: false
+},{
+	id: "012",
+	name: "ДВП",
+	dimension: "516 х 2070 мм.",
+	thickness: 3,
+	producer: "",
+	description: "Деталь у хорошому стані",
+	price: " грн.",
+	available: false
+},{
+	id: "013",
+	name: "ДСП",
+	dimension: "2800 х 958 мм.",
+	thickness: 16,
+	producer: "",
+	description: "Деталь у хорошому стані",
+	price: " грн.",
+	available: false
+},{
+	id: "014",
+	name: "ДСП",
+	dimension: "2750 х 1308 мм.",
+	thickness: 18,
+	producer: "",
+	description: "Деталь у хорошому стані",
+	price: " грн.",
+	available: false
+},{
+	id: "015",
+	name: "ДСП",
+	dimension: "2800 х 585 мм.",
+	thickness: 18,
+	producer: "",
+	description: "Деталь у хорошому стані",
+	price: " грн.",
+	available: false
+},{
+	id: "016",
 	name: "ДСП U 330 ST 15 Баклажан ",
 	dimension: "2800 х 730 мм.",
 	thickness: 18,
 	producer: "Egger",
 	description: "Деталь у хорошому стані",
 	price: "400 грн.",
-	available: true
+	available: false
 }
-]
-
-console.log(data[1]);
-
+];
